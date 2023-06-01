2a34264cec65942ca9ea77a97d9eb4617ecebf4f
Yuriko <ygabriela.arnott@gmail.com>
2023-06-01 15:36:41 -0400

    example

--- todoApp/app.py
+++ todoApp/app.py
@@ -1,16 +1,25 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://postgres:abc@localhost:5432/todoapp'
+db = SQLAlchemy(app)
+
+class Todo(db.Model):
+  __tablename__ = 'todos'
+  id = db.Column(db.Integer, primary_key=True)
+  description = db.Column(db.String(), nullable=False)
+
+  def __repr__(self):
+    return f'<Todo {self.id} {self.description}>'
+
+db.create_all()
 
 @app.route('/')
-def index() -> str:
-    todos = [
-        {'description': 'Todo 1'},
-        {'description': 'Todo 2'},
-        {'description': 'Todo 3'}
-    ]
-    return render_template('index.html', todos=todos)
+def index():
+  return render_template('index.html', data=Todo.query.all())
 
-# Always include this at the bottom of your code (port 3000 is only necessary in workspaces)
+
+#always include this at the bottom of your code
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=3000)
+   app.run(host="0.0.0.0", port=3000)
